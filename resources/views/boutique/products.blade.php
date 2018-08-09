7bb5996b74d72d56780bea2fd080430308e4d76c
DESKTOP-LVN0F53\pranz <nikkimoda@ymail.com>
2018-08-09 22:55:43 +0800

    made changes in routes and products view in boutique side

--- resources/views/boutique/products.blade.php
+++ resources/views/boutique/products.blade.php
@@ -32,103 +32,8 @@
         </div>
     </div>
 
-
-    <!-- <div class="row"> -->
-    	<!-- @foreach($products as $product)  -------------WALA PAKO NAHUMAN ARIIII---------------- -->
-            <!--         {{$product->productFile}} -->
-<!-- Single Product -->
- <!--        <div class="col-md-12 col-lg-3">
-            <div class="single-product-wrapper"> -->
-                <!-- Product Image -->
-               <!--  <div class="product-img">
-
-                <?php 
-                $counter = 1;
-                ?> 
-
-                @foreach( $product->productFile as $image)   
-        
-                @if($counter == 1)
-                    <img src="{{ asset('/uploads').$image['filename'] }}" alt="">
-                @elseif($counter == 2) -->
-                    <!-- Hover Thumb -->
-                    <!-- <img class="hover-img" src="{{ asset('/uploads').$product->productFile  }}" alt=""> -->
-                   <!--  @endif
-                    <?php $counter++; ?>
-                @endforeach -->
-                
-                <!-- </div> -->
-
-                <!-- Product Description --><!-- 
-                <div class="product-description">
-                    <span>{{ $product->owner['username'] }}</span>
-                    <a href="single-product-details.html">
-                        <h6>{{ $product['productName'] }}</h6>
-                    </a>
-                    <p class="product-price">{{ $product['productPrice'] }}</p> -->
-
-                    <!-- Hover Content -->
-                    <!-- <div class="hover-content"> -->
-                        <!-- Add to Cart -->
-           <!--              <div class="add-to-cart-btn">
-                            <a href="viewproduct/{{ $product['productID'] }}" class="btn essence-btn">View Product</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div> -->
-    <!-- Single Product End -->
-		<!-- @endforeach -->
-
-<!-- <br><br> -->
-<!-- </div> -->
-
-<!-- <div class="row">
-@foreach($products as $product)
-
-<div class="col-md-6">
-          <div class="box box-solid">
-            <div class="box-header with-border">
-              <h3 class="box-title">Carousel</h3>
-            </div>
-            <!-- /.box-header -->
-            <!-- <div class="box-body">
-              <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-           
-                <div class="carousel-inner">
-            @foreach( $product->productFile as $image)
-                
-                  <div class="item">
-                    <img src="{{ asset('/uploads').$image['filename'] }}">
-
-                  </div>
-
-
-
-            @endforeach
-                </div>
-
-                <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
-                  <span class="fa fa-angle-left"></span>
-                </a>
-                <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
-                  <span class="fa fa-angle-right"></span>
-                </a>
-                
-              </div>
-            </div> -->
-            <!-- /.box-body -->
-        
-          <!-- </div> -->
-          <!-- /.box -->
-        <!-- </div> -->
-        <!-- /.col -->
-        <!-- @endforeach -->
-        <!-- </div> --> 
-
-
-
 </div>
+
 
 <!-- ------------------------NEW OPTION------------------------------ -->
 
@@ -140,8 +45,9 @@
 
     <div class="col-md-3">
 
-        <div class="box">
-        	<div style="padding: 20px;">
+        <div class="box" style="padding: 20px;">
+        <div style="padding-right: 20px; padding-left: 20px;">
+        <div class="row" style="width: auto; height: 400px;">
 
         <?php 
             $counter = 1;
@@ -150,13 +56,16 @@
         @foreach( $product->productFile as $image)
 
         @if($counter == 1)
-            <img src="{{ asset('/uploads').$image['filename'] }}" style="width: 100%; align-self: center;">
+            <img src="{{ asset('/uploads').$image['filename'] }}" style="max-width: 100%; max-height: 100%; ">
         @else
             
         @endif
 
         <?php $counter++; ?>
         @endforeach
+        </div>
+
+        <div class="row">
 
         <a href="single-product-details.html">
             <h6>{{ $product['productName'] }}</h6>
@@ -164,13 +73,6 @@
 
         <h2></h2>
 
-        <style type="text/css">
-
-        .hover{
-          
-        }
-
-        </style>
 
         <div class="hover">
             <!-- Add to Cart -->
@@ -179,6 +81,7 @@
             </div>
         </div>
 
+        </div>
     	</div>
         </div>
     </div>
