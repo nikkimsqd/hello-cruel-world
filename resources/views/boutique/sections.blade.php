e4920f885d3e79a239857ef9bf62faad65cc6abc
NIKKI\nikki <nikkimoda@ymail.com>
2019-04-04 00:03:57 +0800

    changed boutique avatar

--- resources/views/boutique/sections.blade.php
+++ resources/views/boutique/sections.blade.php
@@ -32,7 +32,7 @@
         <li><!-- start message -->
           <a href="#">
             <div class="pull-left">
-              <img src="{{asset('adminlte/dist/img/user2-160x160.jpg')}}" class="img-circle" alt="User Image">
+              <img src="{{asset('adminlte/dist/img/avatar2.png')}}" class="img-circle" alt="User Image">
             </div>
             <h4>
               Customer Name
@@ -119,13 +119,13 @@
 @section('useraccount')
 <li class="dropdown user user-menu">
   <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-    <img src="{{ asset('adminlte/dist/img/user2-160x160.jpg') }}" class="user-image" alt="User Image">
+    <img src="{{ asset('adminlte/dist/img/avatar2.png') }}" class="user-image" alt="User Image">
     <span class="hidden-xs">{{$boutique['boutiqueName']}}</span>
   </a>
   <ul class="dropdown-menu">
     <!-- User image -->
     <li class="user-header">
-      <img src="{{ asset('adminlte/dist/img/user2-160x160.jpg') }}" class="img-circle" alt="User Image">
+      <img src="{{ asset('adminlte/dist/img/avatar2.png') }}" class="img-circle" alt="User Image">
 
       <p>
         {{$boutique['boutiqueName']}}
@@ -170,7 +170,7 @@
   <!-- Sidebar user panel -->
   <div class="user-panel">
     <div class="pull-left image">
-      <img src="{{ asset('adminlte/dist/img/user2-160x160.jpg') }}" class="img-circle" alt="User Image">
+      <img src="{{ asset('adminlte/dist/img/avatar2.png') }}" class="img-circle" alt="User Image">
     </div>
     <div class="pull-left info">
       <p>{{$boutique['boutiqueName']}}</p>
--- resources/views/hinimo/single-product-details.blade.php
+++ resources/views/hinimo/single-product-details.blade.php
@@ -104,8 +104,8 @@
           <!-- Modal content-->
           <div class="modal-content">
             <div class="modal-header">
-              <button type="button" class="close" data-dismiss="modal">&times;</button>
               <h3 class="modal-title"><b>Rent Details</b></h3>
+              <button type="button" class="close" data-dismiss="modal">&times;</button>
             </div>
 
             <div class="modal-body">
